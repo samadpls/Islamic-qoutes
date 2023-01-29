6611c83ae57678c821abd798d3d845f729e5ac21
samadpls <abdulsamadsid1@gmail.com>
2023-01-29 19:07:51 +0500

    working

--- index.js
+++ index.js
@@ -8,35 +8,41 @@ const port = process.env.PORT | 3000
 // Create an HTTP server that listens for incoming requests on port 8080
 // http.createServer(function(request, response) {
 
-const server=http.createServer((request,response)=>{
-  // If the request is for the root url "/", serve the image file
-  if (request.url == "/") {
-    // Read the image file from the filesystem
-	fs.readdir(testFolder,(err,files)=>{
-		var fileName=files[Math.floor(Math.random()*files.length)];
-		console.log(fileName)
-	fs.readFile("./static/images/"+fileName, function(err, data) {
-		if (err) {
-			// If there was an error reading the file, send a 404 Not Found response
-			response.writeHead(404);
-			response.end();
-			return;
-		}
-	
-		// Otherwise, send the image file data in the response
-		response.writeHead(200, { "Content-Type": "image/jpeg" });
-		response.end(data);
-		});
-		
-	})
-   
-  }
+const server = http.createServer((request, response) => {
+	// If the request is for the root url "/", serve the image file
+	if (request.url == "/") {
+		// Read the image file from the filesystem
+		fs.readdir(testFolder, (err, files) => {
+			var fileName = files[Math.floor(Math.random() * files.length)];
+			console.log(fileName)
+			fs.readFile("./static/images/" + fileName, function (err, data) {
+				if (err) {
+					// If there was an error reading the file, send a 404 Not Found response
+					response.writeHead(404);
+					response.end();
+					return;
+				}
+
+				// Otherwise, send the image file data in the response
+				response.writeHead(200, {
+					"Content-Type": "image/jpeg",
+					"Cache-Control": "no-cache, no-store, must-revalidate",
+					"Pragma": "no-cache",
+					"Expires": "0"
+				});
+				response.end(data);
+
+			});
+
+		})
+
+	}
 })
 
 
 
-server.listen(port, () => {console.log('Server Started');});
+server.listen(port, () => { console.log('Server Started'); });
 // fs.readdir(testFolder, (err, files) => {
 // 	var fileName= files[Math.floor(Math.random()*files.length)];
-// 	console.log(fileName)  
+// 	console.log(fileName)
 //   });
